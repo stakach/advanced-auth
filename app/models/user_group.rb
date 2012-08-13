5fbfd8e53c8dd5c64a0fc5b53addafee8b76cb06
Steve von Takach <steve@advancedcontrol.com.au>
2012-08-13 14:21:59 +1000

    Destroy user if not in any groups

--- app/models/user_group.rb
+++ app/models/user_group.rb
@@ -2,7 +2,25 @@ class UserGroup < ActiveRecord::Base
 	belongs_to :group
 	belongs_to :user
 	
+	
 	attr_accessible :user_id, :group_id, :permissions, :notes
+	
+	
+	after_destroy :check_user
+	
+	
+	
+	protected
+	
+	
+	
+	def check_user
+		if !UserGroup.where(:user_id => self.user_id).exists?
+			User.destroy(self.user_id)
+		end
+	end
+	
+	
 	
 	validates_presence_of :user_id, :group_id, :permissions
 	validates :user_id, :uniqueness => {:scope => :group_id,	:message => "This user is already a group member"}
